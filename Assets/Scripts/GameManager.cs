7b979dc07b706b04df4569c5b1b5aa0aefff864a
Loann <courcoul66@gmail.com>
2022-06-07 16:08:36 +0200

    When we click on the first enemy he disappear

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public ScoreManager scoreManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Camera _cam;
+
+	private void Start()
+	{
+		_cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out var hit))
+			{
+				if (hit.collider.name == scoreManager.enemiesInGoodOrder[0].name)
+				{
+					hit.collider.TryGetComponent(out ScoreEnemy scoreEnemy);
+					
+					scoreManager.enemiesInGoodOrder.Remove(scoreEnemy);
+					
+					hit.collider.gameObject.SetActive(false);
+				}
+			}
+		}
+	}
 }
