75ef37fc8ad77e463539b2aab133f435c6a747f4
Timothé <rondeautimothe@gmail.com>
2022-06-07 14:58:57 +0200

    Enemy health and can take damage

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,27 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
-    [SerializeField]
-    private int number;
-    [SerializeField]
-    private TextMeshProUGUI textNumber;
     
-    
+    public EnemyHealth enemyHealth;
+  
     [Header("MOVEMENT PARAMETERS")]
     [SerializeField] private Transform target;
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float rangeMovement = 1f;
-   protected virtual void Start()
+
+    protected virtual void Awake()
+    {
+        enemyHealth = new EnemyHealth();
+    }
+
+    protected virtual void Start()
    {
        target = GameObject.FindWithTag("Player").transform;
+       enemyHealth.EnemyDeathEvent += OnEnemyDeathEvent;
    }
 
   
+
    protected virtual void Update()
    {
         MoveToPlayer();
@@ -41,5 +44,12 @@ public class Enemy : MonoBehaviour
        var lookForward = target.position - transform.position;
        transform.rotation = Quaternion.LookRotation(lookForward, Vector3.up);
    }
-    
+
+   protected virtual void OnEnemyDeathEvent()
+   {
+       //TODO VFX explosion enemy
+       Destroy(gameObject);
+   }
+
+  
 }
