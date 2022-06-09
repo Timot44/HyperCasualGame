3943a1dc4aae1ab573182fbf84a7609c338110fa
Timothé <rondeautimothe@gmail.com>
2022-06-09 20:13:54 +0200

    Update Canvas ui

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+    
+}
