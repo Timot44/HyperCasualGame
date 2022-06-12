57cd59238ef9ebd3db66e9498356d9319e188b0d
Timothé <rondeautimothe@gmail.com>
2022-06-12 16:43:51 +0200

    Testing For gif

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && EnemySpawnerManager.Instance.isAllEnemiesSpawned)
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
