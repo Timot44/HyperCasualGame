632509814f302bb7a82b0202907bb99f0302525e
Timothé <rondeautimothe@gmail.com>
2022-06-13 13:26:29 +0200

    Add wave count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
 
 
     public TextMeshPro textScore;
-
+    public TextMeshPro textWave;
     private float _multiplierToAddMax;
 
     private Camera cam;
